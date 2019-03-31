087430e87f07e99237d62a21a51f2e46d018af5e
Raphaenn <38678724+Raphaenn@users.noreply.github.com>
2019-03-31 10:55:22 -0300

    Mudanças nas funcoes

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,34 +1,12 @@
-/* var currencyInput = document.querySelector('input[type="currency"]');
-var currency = 'GBP'; // https://www.currency-iso.org/dam/downloads/lists/list_one.xml
 
-currencyInput.addEventListener('focus', onFocus);
-currencyInput.addEventListener('blur', onBlur);
+// Função para somar envios dos fornecedores detalhados
+var table = document.getElementById("Fat_table"), sumVal = 0;
 
-function localStringToNumber( s ){
-    return Number(String(s).replace(/[^0-9.-]+/g,""));
-}
-
-function onFocus(e){
-  var value = e.target.value;
-  e.target.value = value ? localStringToNumber(value) : '';
-}
-
-function onBlur(e){
-  var value = e.target.value;
-
-  const options = {
-      maximumFractionDigits : 2,
-      currency              : currency,
-      style                 : "currency",
-      currencyDisplay       : "symbol"
+  for (var i = 1; i < table.rows.length; i++) {
+    sumVal = sumVal + parseInt(table.rows[i].cells[6].innerHTML);
   }
-  
-  e.target.value = value 
-    ? localStringToNumber(value).toLocaleString(undefined, options)
-    : ''
-};
 
-*/
+document.getElementById("dinheiro").innerHTML = "R$"+sumVal;
 
 
 // Função para calculo do imc - Teste de funções
@@ -84,5 +62,37 @@ if (alturaValida && pesoValido) {
 }
 
 console.log(imc);
+
+*/
+
+/* var currencyInput = document.querySelector('input[type="currency"]');
+var currency = 'GBP'; // https://www.currency-iso.org/dam/downloads/lists/list_one.xml
+
+currencyInput.addEventListener('focus', onFocus);
+currencyInput.addEventListener('blur', onBlur);
+
+function localStringToNumber( s ){
+    return Number(String(s).replace(/[^0-9.-]+/g,""));
+}
+
+function onFocus(e){
+  var value = e.target.value;
+  e.target.value = value ? localStringToNumber(value) : '';
+}
+
+function onBlur(e){
+  var value = e.target.value;
+
+  const options = {
+      maximumFractionDigits : 2,
+      currency              : currency,
+      style                 : "currency",
+      currencyDisplay       : "symbol"
+  }
+  
+  e.target.value = value 
+    ? localStringToNumber(value).toLocaleString(undefined, options)
+    : ''
+};
 
 */
